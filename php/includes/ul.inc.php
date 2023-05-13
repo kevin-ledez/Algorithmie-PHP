fa9ad9133a4b2afdb52fcfdbb65072e13558e529
kevin-ledez <kevin.ledez@gmail.com>
2023-05-13 18:12:53 +0200

    ADD Form

---
+++ php/includes/ul.inc.php
@@ -0,0 +1,8 @@
+<div class="list-group">
+    <a href="index.php?page=home" class="list-group-item list-group-item-action active" aria-current="true">Home</a>
+    <a href="index.php?page=debugging" class="list-group-item list-group-item-action">Débogage</a>
+    <a href="index.php?page=concatenation" class="list-group-item list-group-item-action">Concaténation</a>
+    <a href="index.php?page=loop" class="list-group-item list-group-item-action">Boucle</a>
+    <a href="index.php?page=function" class="list-group-item list-group-item-action">Fonction</a>
+    <a href="index.php?page=del" class="list-group-item list-group-item-action">Supprimer</a>
+</div>
--- php/index.php
+++ php/index.php
@@ -1,42 +1,43 @@
 <?php
+session_start();
 $url = $_GET['page'];
 include "./includes/head.inc.html";
 include "./includes/header.inc.html";
 ?>
 
 <section>
-    <?php
-    if ($url == "home")
-        include "";
-    else if ($url == "debugging")
-        include "";
-    else if ($url == "concatenation")
-        include "";
-    else if ($url == "loop")
-        include "";
-    else if ($url == "fonction")
-        include "";
-    else if ($url == "del")
-        include "";
-    ?>
-
     <div class="container">
         <div class="row">
             <div class="col">
-                <div class="list-group">
-                    <a href="#" class="list-group-item list-group-item-action active" aria-current="true">Home</a>
-                    <a href="#" class="list-group-item list-group-item-action">Débogage</a>
-                    <a href="#" class="list-group-item list-group-item-action">Concaténation</a>
-                    <a href="#" class="list-group-item list-group-item-action">Boucle</a>
-                    <a href="#" class="list-group-item list-group-item-action">Fonction</a>
-                    <a href="#" class="list-group-item list-group-item-action">Supprimer</a>
-                </div>
+                <?php include "./includes/ul.inc.php" ?>
             </div>
             <div class="col">
-                <button type="button" class="btn btn-primary">Ajouter des données</button>
-            </div>
-            <div class="col">
-                <button type="button" class="btn btn-secondary">Ajouter plus de données</button>
+<?php
+if ($url == "add")
+        include "./includes/form.inc.html";
+    else if ($url == "debugging"){
+        echo "<h2>Débogage</h2><br>";
+            print "<pre>";
+            print_r ($table);
+            print "</pre>";
+    }
+    else if ($url == "concatenation"){
+        echo "<h2>Concaténation</h2><br>";
+    }
+    else if ($url == "loop"){
+        echo "<h2>Boucle</h2><br>";
+    }
+    else if ($url == "function"){
+        echo "<h2>Fonction</h2><br>";
+    }
+    else if ($url == "del"){
+        echo "<h2>Données supprimées</h2>";
+    }
+    else {
+        echo "<a href='index.php?page=add'><button type='button' class='btn btn-primary'>Ajouter des données</button></a>";
+        echo "<a type='button' class='btn btn-secondary'>Ajouter plus de données</a>";
+    }
+?>    
             </div>
         </div>
     </div>
