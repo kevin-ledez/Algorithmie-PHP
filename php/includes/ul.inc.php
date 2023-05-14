7f4b5addb0c7fc49158e56426897397c64dfa787
kevin-ledez <kevin.ledez@gmail.com>
2023-05-14 12:19:19 +0200

    Added form processing

--- php/includes/ul.inc.php
+++ php/includes/ul.inc.php
@@ -1,8 +1,8 @@
 <div class="list-group">
-    <a href="index.php?page=home" class="list-group-item list-group-item-action active" aria-current="true">Home</a>
-    <a href="index.php?page=debugging" class="list-group-item list-group-item-action">Débogage</a>
-    <a href="index.php?page=concatenation" class="list-group-item list-group-item-action">Concaténation</a>
-    <a href="index.php?page=loop" class="list-group-item list-group-item-action">Boucle</a>
-    <a href="index.php?page=function" class="list-group-item list-group-item-action">Fonction</a>
-    <a href="index.php?page=del" class="list-group-item list-group-item-action">Supprimer</a>
+    <a href="index.php" class="list-group-item list-group-item-action active" aria-current="true">Home</a>
+    <a href="index.php?debugging" class="list-group-item list-group-item-action">Débogage</a>
+    <a href="index.php?concatenation" class="list-group-item list-group-item-action">Concaténation</a>
+    <a href="index.php?loop" class="list-group-item list-group-item-action">Boucle</a>
+    <a href="index.php?function" class="list-group-item list-group-item-action">Fonction</a>
+    <a href="index.php?del" class="list-group-item list-group-item-action">Supprimer</a>
 </div>
--- php/index.php
+++ php/index.php
@@ -1,40 +1,59 @@
 <?php
 session_start();
-$url = $_GET['page'];
+$table = $_SESSION['table'];
 include "./includes/head.inc.html";
 include "./includes/header.inc.html";
 ?>
 
 <section>
     <div class="container">
+        <?php print_r($_SESSION); ?>
         <div class="row">
             <div class="col">
                 <?php include "./includes/ul.inc.php" ?>
             </div>
             <div class="col">
 <?php
-if ($url == "add")
+    if (isset($_GET['add'])){
         include "./includes/form.inc.html";
-    else if ($url == "debugging"){
+    }else if (isset($_POST['register_data'])){
+        $prenom = $_POST['prenom'];
+        $nom = $_POST['nom'];
+        $age = $_POST['age'];
+        $taille = $_POST['taille'];
+        $genre = $_POST['genre'];
+
+        $table = array(
+            'first_name' => $prenom,
+            'last_name' => $nom,
+            'age' => $age,
+            'size' => $taille,
+            'genre' => $genre,
+        );
+        $_SESSION['table'] = $table;
+        echo "<h2>Données sauvegardées</h2>";
+    }
+    else if (isset($_GET['debugging'])){
         echo "<h2>Débogage</h2><br>";
             print "<pre>";
             print_r ($table);
             print "</pre>";
     }
-    else if ($url == "concatenation"){
+    else if (isset($_GET['concatenation'])){
         echo "<h2>Concaténation</h2><br>";
     }
-    else if ($url == "loop"){
+    else if (isset($_GET['loop'])){
         echo "<h2>Boucle</h2><br>";
     }
-    else if ($url == "function"){
+    else if (isset($_GET['function'])){
         echo "<h2>Fonction</h2><br>";
     }
-    else if ($url == "del"){
+    else if (isset($_GET['del'])){
+        unset($_SESSION['table']);
         echo "<h2>Données supprimées</h2>";
     }
     else {
-        echo "<a href='index.php?page=add'><button type='button' class='btn btn-primary'>Ajouter des données</button></a>";
+        echo "<a href='index.php?add'><button type='button' class='btn btn-primary'>Ajouter des données</button></a>";
         echo "<a type='button' class='btn btn-secondary'>Ajouter plus de données</a>";
     }
 ?>    
