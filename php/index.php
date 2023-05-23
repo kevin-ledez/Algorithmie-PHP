64408ea0d7f3e8f88f76b3bd92ad68bf82da1f4a
Kevin LEDEZ <kevin.ledez@gmail.com>
2023-05-23 15:37:45 +0200

    Add Function readSex for civility + date

--- php/index.php
+++ php/index.php
@@ -7,7 +7,7 @@ include "./includes/header.inc.html";
 <section>
     <div class="container">
         <div class="row">
-            <div class="col">
+            <div class="col-md-2">
                 <a href="index.php" class="list-group-item list-group-item-action" aria-current="true">Home</a>
                 <?php 
                     if (!empty($_SESSION)){
@@ -25,7 +25,7 @@ include "./includes/header.inc.html";
     else if (isset($_GET['addmore'])){
         include "./includes/form2.inc.php";
     }
-    else if (isset($_POST['register_data'])){
+    else if (isset($_POST['register_data']) || isset($_POST['register_data_more'])){
         $prenom = $_POST['prenom'];
         $nom = $_POST['nom'];
         $age = $_POST['age'];
@@ -40,11 +40,14 @@ include "./includes/header.inc.html";
         $bootstrap = isset($_POST['bootstrap']) ? $_POST['bootstrap'] : '';
         $symfony = isset($_POST['symfony']) ? $_POST['symfony'] : '';
         $react = isset($_POST['react']) ? $_POST['react'] : '';
-        
-        $filepath = 'uploaded/' . $_FILES['image']['name'];
+        $dob = isset($_POST['dob']) ? $_POST['dob'] : '';
 
+        $filepath = 'uploaded/' . $_FILES['image']['name'];
+        
         if (move_uploaded_file($_FILES['image']['tmp_name'], $filepath)) {
+            echo '<div class="alert alert-success text-center" role="alert">';
             echo "Image enregistrée";
+            echo '</div>';
         }else{
             echo "Image non enregistrée";
         }
@@ -70,6 +73,7 @@ include "./includes/header.inc.html";
             'bootstrap' => $bootstrap,
             'symfony' => $symfony,
             'react' => $react,
+            'dob' => $dob,
             'img' => array(
                 'name' => $name,
                 'type' => $extension,
@@ -88,37 +92,25 @@ include "./includes/header.inc.html";
         echo "<h2>Débogage</h2>";
         echo "===> Lecture du tableau à l'aide de la fonction print_r()";
             print "<pre>";
-            print_r ($table);
+            print_r(array_filter($table));
             print "</pre>";
     }
     else if (isset($_GET['concatenation'])){
         echo "<h2>Concaténation</h2>";
-        echo "===> Construction d'une phrase avec le contenu du tableau";
-        if ($table['civility'] == 'homme') {
-            echo "<p>Mr ".$table['first_name']." ".$table['last_name']."<br>";
-        }else{
-            echo "<p>Mme ".$table['first_name']." ".$table['last_name']."<br>";
-        }
-        echo "J'ai ".$table['age']." ans et je mesure ".$table['size']." m.</p>";
+        echo "===> Construction d'une phrase avec le contenu du tableau<br>";
+        echo readSex($table)." ".$table['first_name']." ".$table['last_name']."<br>";
+        echo "J'ai ".$table['age']." ans et je mesure ".$table['size']." m.<br><br>";
 
-        echo "===> Construction d'une phrase après MAJ du tableau";
+        echo "===> Construction d'une phrase après MAJ du tableau<br>";
         $table['first_name'] = ucfirst($table['first_name']);
         $table['last_name'] = strtoupper($table['last_name']);
-        if ($table['civility'] == 'homme') {
-            echo "<p>Mr ".$table['first_name']." ".$table['last_name']."<br>";
-        }else{
-            echo "<p>Mme ".$table['first_name']." ".$table['last_name']."<br>";
-        }
-        echo "J'ai ".$table['age']." ans et je mesure ".$table['size']." m.</p>";
+        echo readSex($table)." ".$table['first_name']." ".$table['last_name']."<br>";
+        echo "J'ai ".$table['age']." ans et je mesure ".$table['size']." m.<br><br>";
         
-        echo "===> Affichage d'une virgule à la place du point pour la taille";
+        echo "===> Affichage d'une virgule à la place du point pour la taille<br>";
         $table['size'] = str_replace('.',',',$table['size']);
-        if ($table['civility'] == 'homme') {
-            echo "<p>Mr ".$table['first_name']." ".$table['last_name']."<br>";
-        }else{
-            echo "<p>Mme ".$table['first_name']." ".$table['last_name']."<br>";
-        }
-        echo "J'ai ".$table['age']." ans et je mesure ".$table['size']." m.</p>";
+        echo readSex($table)." ".$table['first_name']." ".$table['last_name']."<br>";
+        echo "J'ai ".$table['age']." ans et je mesure ".$table['size']." m.<br><br>";
     }
     else if (isset($_GET['loop'])){
         echo "<h2>Boucle</h2>";
@@ -149,6 +141,14 @@ include "./includes/header.inc.html";
         $x = 0;
         foreach ($table as $key => $value) {
             echo '<p>à la ligne n°'.$x++.' corresponds à clé "'.$key.'" et valeur "'.$value.'"</p>';
+        }
+    }
+
+    function readSex($table){
+        if ($table['civility'] == 'homme') {
+            echo "Mr";
+        }else{
+            echo "Mme";
         }
     }
 ?>
